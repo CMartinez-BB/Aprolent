87877a80edd8f4705b5152de943924e64db2e228
CMartinez-BB <charli.rey.misterio@gmail.com>
2022-11-24 14:28:02 -0600

    Adding header to Login and register

---
+++ components/HeaderLoginR.php
@@ -0,0 +1,11 @@
+<!-- Header - Menu -->
+<header class="containe-head">
+
+<div id="logo">
+    DeveroCode
+</div>
+<!-- Nav -->
+<nav class="menu-bar">
+    <a href="../../index.php">Inicio</a>    
+</nav>
+</header>
