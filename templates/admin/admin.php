5c6c2d2177be7e5e69a1f0ec6e7ed670f375da13
CMartinez-BB <mtz.carlos.123.nz@gmail.com>
2022-11-26 22:04:24 -0700

    fixed bugs

--- templates/admin/admin.php
+++ templates/admin/admin.php
@@ -328,7 +328,6 @@
                                 <a class="dropdown-item" href="#">
                                     <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
                                     Activity Log
-                                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                                 </a>
                                 <div class="dropdown-divider"></div>
                                 <a class="dropdown-item" href="../../php/logoutController.php">
